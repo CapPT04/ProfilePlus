48331b662e6a5c47c04d77631e0dd4fc82c3a9de
CapPT04 <144525255+CapPT04@users.noreply.github.com>
2023-10-02 00:11:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,39 @@
-let elToShow = document.querySelectorAll('.show-on-scroll')
-
-let isElInViewPort = (el) => {
-	let rect = el.getBoundingClientRect()
-	// some browsers support innerHeight, others support documentElement.clientHeight
-	let viewHeight = window.innerHeight || document.documentElement.clientHeight
-
-	return (
-		(rect.top <= 0 && rect.bottom >= 0) ||
-		(rect.bottom >= viewHeight && rect.top <= viewHeight) ||
-		(rect.top >= 0 && rect.bottom <= viewHeight)
-	)
-}
-
-function loop() {
-	elToShow.forEach((item) => {
-		if (isElInViewPort(item)) {
-			item.classList.add('start')
-		} else {
-			item.classList.remove('start')
-		}
-	})
-}
-
-window.onscroll = loop
-
-loop()
+let textLogo = document.querySelector('.logo')
+const tl = gsap.timeline({
+    defaults: {
+        ease: "power4.inOut"
+    }
+})
+textLogo.innerHTML = textLogo.textContent.replace (
+    /\S/g,
+    "<span class='letter'>$&</span>"
+    
+)
+tl.from('.letter', 0.7, {
+    y: -50,
+    x: 50,
+    rotation: 20,
+    autoAlpha: 0,
+    stagger: 0.1,
+})
+.from(
+    '.nav_list',
+    0.7,
+    {
+        y: 50,
+        x: -50,
+        rotation: 180,
+        autoAlpha: 0,
+        stagger: 0.1,
+    },
+    '-=1'
+)
+.from(
+    '.home_left',
+    1.5,
+    {
+        x: '-100%',
+        autoAlpha: 0,
+    },
+    '-=1'
+)
